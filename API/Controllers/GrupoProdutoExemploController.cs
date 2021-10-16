4717d76e0c7d678af0c671d6d4cd6ac1d0524966
Rafael <silvagpe@gmail.co>
2021-10-16 14:26:16 -0300

    grupo produto

---
+++ API/Controllers/GrupoProdutoExemploController.cs
@@ -0,0 +1,77 @@
+using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UStart.API.Controllers
+{
+    /// <summary>
+    /// Exemplo de controller
+    /// </summary>
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/grupo-produto-exemplo")]
+    //[Authorize]
+    public class GrupoProdutoExemploController : ControllerBase
+    {
+        public GrupoProdutoExemploController()
+        {            
+        }
+
+        /// <summary>
+        /// Consultar todos os grupos
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]        
+        public IActionResult GetGruposProdutos()
+        {
+            return Ok("consulta de grupos");
+        }
+
+        /// <summary>
+        /// Consultar apenas um grupo
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]    
+        [Route("{id}")]    
+        public IActionResult GetGrupoProduto([FromRoute] string id)
+        {
+            return Ok(string.Format("consulta um grupo: {0}", id));
+        }
+
+        /// <summary>
+        /// Adicionar (insert) um grupo
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        [HttpPost]            
+        public IActionResult AdicionarGrupoProduto([FromBody] dynamic command)
+        {
+            return Ok(string.Format("adicionar um grupo: {0}", command));
+        }
+
+        /// <summary>
+        /// Atualizar (update) um grupo
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        [HttpPut]            
+        public IActionResult AtualizarGrupoProduto([FromBody] dynamic command)
+        {
+            return Ok(string.Format("atualizar um grupo: {0}", command));
+        }
+
+        /// <summary>
+        /// Excluir um grupo por Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete("{id}")]            
+        public IActionResult DeletarGrupoProduto([FromRoute] string id)
+        {
+            return Ok(string.Format("excluir um grupo: {0}", id));
+        }
+
+
+    }
+}
